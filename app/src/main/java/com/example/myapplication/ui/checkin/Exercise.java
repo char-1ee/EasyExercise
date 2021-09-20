305fe05910491fbbbb989c9ab49f18f2a15fc6b4
Ruan Donglin <RUAN0018@e.ntu.edu.sg>
2021-09-20 21:23:07 +0800

    Just for test..

---
+++ app/src/main/java/com/example/myapplication/ui/checkin/Exercise.java
@@ -0,0 +1,113 @@
+package com.example.myapplication.ui.checkin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Exercise extends AppCompatActivity
+{
+    TextView timerText;
+    Button stopStartButton;
+
+
+    Timer timer;
+    TimerTask timerTask;
+    Double time = 0.0;
+
+    boolean timerStarted = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+
+        timerText = (TextView) findViewById(R.id.timerText);
+        stopStartButton = (Button) findViewById(R.id.startStopButton);
+
+        timer = new Timer();
+
+    }
+
+
+//    public void startStopTapped(View view)
+//    {
+//        if(timerStarted == false)
+//        {
+//            timerStarted = true;
+//            setButtonUI("STOP", R.color.red);
+//
+//            startTimer();
+//        }
+//        else
+//        {
+//            timerStarted = false;
+//            setButtonUI("START", R.color.green);
+//
+//            timerTask.cancel();
+//        }
+//    }
+
+    private void setButtonUI(String start, int color)
+    {
+        stopStartButton.setText(start);
+        stopStartButton.setTextColor(ContextCompat.getColor(this, color));
+    }
+
+    private void startTimer()
+    {
+        timerTask = new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        time++;
+                        timerText.setText(getTimerText());
+                    }
+                });
+            }
+
+        };
+        timer.scheduleAtFixedRate(timerTask, 0 ,1000);
+    }
+
+
+    private String getTimerText()
+    {
+        int rounded = (int) Math.round(time);
+
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = ((rounded % 86400) / 3600);
+
+        return formatTime(seconds, minutes, hours);
+    }
+
+    private String formatTime(int seconds, int minutes, int hours)
+    {
+        return String.format("%02d",hours) + " : " + String.format("%02d",minutes) + " : " + String.format("%02d",seconds);
+    }
+
+}

eb0a8f4be441f21a607d815b1eeebf7b313946ee
Zhong Ruoyu <zhongruoyu@outlook.com>
2021-10-02 21:54:49 +0800

    Class Coordinates updated
    Co-Authored-By: Steph727 <70131163+Steph727@users.noreply.github.com>

--- app/src/main/java/com/example/myapplication/beans/Coordinates.java
+++ app/src/main/java/com/example/myapplication/beans/Coordinates.java
@@ -1,6 +1,10 @@
 package com.example.myapplication.beans;
 
+import java.util.Locale;
+
 public class Coordinates {
+    private static final double R = 6371; // radius of the earth in km
+
     private final double latitude;
     private final double longitude;
     private final String name;
@@ -15,6 +19,30 @@ public class Coordinates {
         this(latitude, longitude, "");
     }
 
+    public double getDistance(Coordinates coordinates) {
+        return getDistance(coordinates.latitude, coordinates.longitude);
+    }
+
+    /**
+     * Get the distance from the current location to another.
+     *
+     * @param latitude the latitude of the other location
+     * @param longitude the longitude of the other location
+     * @return the distance between the two locations in kilometres
+     * @see http://www.movable-type.co.uk/scripts/latlong.html
+     */
+    public double getDistance(double latitude, double longitude) {
+        double dLatitude = Math.toRadians(this.latitude - latitude);
+        double dLongitude = Math.toRadians(this.longitude - longitude);
+        double a = Math.sin(dLatitude / 2) * Math.sin(dLatitude / 2)
+                + Math.cos(Math.toRadians(this.latitude))
+                * Math.cos(Math.toRadians(latitude))
+                * Math.sin(dLongitude / 2) * Math.sin(dLongitude / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c;
+        return d;
+    }
+
     public double getLatitude() {
         return latitude;
     }
@@ -25,5 +53,25 @@ public class Coordinates {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        if (name == null) {
+            return String.format("[%s, %s]", latitudeToString(),
+                    longitudeToString());
+        }
+        return String.format("[%s: %s, %s]", name, latitudeToString(),
+                longitudeToString());
+    }
+
+    public String latitudeToString() {
+        return String.format(Locale.getDefault(), "%f%s", Math.abs(latitude),
+                latitude >= 0 ? "E" : "W");
+    }
+
+    public String longitudeToString() {
+        return String.format(Locale.getDefault(), "%f%s", Math.abs(longitude),
+                longitude >= 0 ? "N" : "S");
     }
 }

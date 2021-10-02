89efc43fd266f1f13744d819bb2aeac1b6730e86
maoyiyun <maoyiyunchris@qq.com>
2021-10-02 12:10:33 +0800

    Finished the UI of fragment me and created an activity for log out page

---
+++ app/src/main/java/com/example/myapplication/ui/activities/EditProfileActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import static androidx.databinding.DataBindingUtil.setContentView;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}

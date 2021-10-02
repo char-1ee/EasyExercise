3a1f397ddefefedc5f67ba3ae55358e4bd8ac717
Zhong Ruoyu <zhongruoyu@outlook.com>
2021-10-02 18:42:53 +0800

    Coordinates class updated
    Co-Authored-By: Steph727 <70131163+Steph727@users.noreply.github.com>

--- app/src/main/java/com/example/myapplication/beans/Coordinates.java
+++ app/src/main/java/com/example/myapplication/beans/Coordinates.java
@@ -1,27 +1,29 @@
 package com.example.myapplication.beans;
 
 public class Coordinates {
-    private double latitude;
-    private double longitude;
+    private final double latitude;
+    private final double longitude;
+    private final String name;
 
-    public Coordinates(double latitude, double longitude) {
+    public Coordinates(double latitude, double longitude, String name) {
         this.latitude = latitude;
         this.longitude = longitude;
+        this.name = name;
+    }
+
+    public Coordinates(double latitude, double longitude) {
+        this(latitude, longitude, "");
     }
 
     public double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
     public double getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
+    public String getName() {
+        return name;
     }
 }

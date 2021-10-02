40d977499aaaee5a57113965aa2175f113ea691d
maoyiyun <maoyiyunchris@qq.com>
2021-10-02 23:44:51 +0800

    tried to complete CheckOutActivity.java, but really unsure about what i need to do

--- app/src/main/java/com/example/myapplication/ui/activities/CheckOutActivity.java
+++ app/src/main/java/com/example/myapplication/ui/activities/CheckOutActivity.java
@@ -1,16 +1,27 @@
 package com.example.myapplication.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.beans.Sport;
+
+import java.util.ArrayList;
 
 public class CheckOutActivity extends AppCompatActivity {
+    private ImageView profileView, sportView;
+    private Button shareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_out);
+        shareButton=findViewById(R.id.shareButton);
+        profileView=findViewById(R.id.checkoutProfile);
+
     }
 }

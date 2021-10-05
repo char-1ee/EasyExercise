a212c2c1601c74ae0b02cae145c6d756defb9d4e
Zhou Yuxuan <rocketzyx@gmail.com>
2021-10-05 14:20:54 +0800

    chatroom, ui finished, logic developing

--- app/src/main/java/com/example/myapplication/ui/activities/ChatRoomActivity.java
+++ app/src/main/java/com/example/myapplication/ui/activities/ChatRoomActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +14,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.myapplication.R;
 import com.example.myapplication.beans.Message;
 import com.example.myapplication.ui.adapters.MessageAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,26 +37,41 @@ public class ChatRoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
 
-        msgList.add(new Message("Hello World!", "Charles"));
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://ontology-5ae5d-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        DatabaseReference mDatabase = database.getReference();
 
         inputText = (EditText) findViewById(R.id.inputText);
         send = (Button) findViewById(R.id.send);
         recyclerView = (RecyclerView) findViewById(R.id.messageRecyclerView);
 
-        Log.w("Chatroom", "TextLog!!!");
-        if(recyclerView == null){
-            Log.wtf("Chatroom", "Woc!!!");
-        }
-        if(recyclerView == null)
-        {
-            Log.wtf("Chatroom", "Noooo!!!");
-        }
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(ChatRoomActivity.this);
         recyclerView.setLayoutManager(layoutManager);
 
         adapter = new MessageAdapter(msgList);
         recyclerView.setAdapter(adapter);
+
+
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.e("chatroom", "Value received!");
+                Message receiveMessage = snapshot.getValue(Message.class);
+                if(receiveMessage != null) {
+                    //Log.w("chatroom", receiveMessage.getMessageText());
+                    msgList.add(receiveMessage);
+                    adapter.notifyItemInserted(msgList.size());
+                    recyclerView.scrollToPosition(msgList.size());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("chatroom", "Failed to read value.", error.toException());
+            }
+        });
 
 
         send.setOnClickListener(new View.OnClickListener(){
@@ -61,10 +82,8 @@ public class ChatRoomActivity extends AppCompatActivity {
                 if(!"".equals(content)){
                     try{
                         Message handleMsg = new Message(content, "Charles");
+                        mDatabase.child("chatroom").child(String.valueOf(msgList.size()+1)).setValue(handleMsg);
                         inputText.setText("");
-                        msgList.add(handleMsg);
-                        adapter.notifyItemInserted(msgList.size()-1);
-                        recyclerView.scrollToPosition(msgList.size()-1);
                     } catch(Exception e){
                         e.printStackTrace();
                     }

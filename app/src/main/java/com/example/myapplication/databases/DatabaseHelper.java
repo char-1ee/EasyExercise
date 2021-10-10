8bf72e7d2fb357d8902feb29da6428835e3ec43e
Char1ee <xingjianli59@gmail.com>
2021-10-10 10:33:48 +0800

    feat: add database helper class

---
+++ app/src/main/java/com/example/myapplication/databases/DatabaseHelper.java
@@ -0,0 +1,46 @@
+package com.example.myapplication.databases;
+
+import static com.example.myapplication.databases.DatabaseContract.*;
+import static com.example.myapplication.databases.DatabaseContract.Facility.*;
+import static com.example.myapplication.databases.DatabaseContract.Sport.*;
+import static com.example.myapplication.databases.DatabaseContract.WorkoutHistory.*;
+import static com.example.myapplication.databases.DatabaseContract.WorkoutPlan.*;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static DatabaseHelper databaseHelper;
+//    private DatabaseHelper databaseHelper;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_SPORT);
+        db.execSQL(CREATE_TABLE_FACILITY);
+        db.execSQL(CREATE_TABLE_WORKOUT_PLAN);
+        db.execSQL(CREATE_TABLE_WORKOUT_HISTORY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DELETE_TABLE_FACILITY);
+        db.execSQL(DELETE_TABLE_SPORT);
+        db.execSQL(DELETE_TABLE_WORKOUT_HISTORY);
+        db.execSQL(DELETE_TABLE_WORKOUT_PLAN);
+        onCreate(db);
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+
+        //enable foreign key constraints like ON UPDATE CASCADE, ON DELETE CASCADE
+        db.execSQL("PRAGMA foreign_keys=ON;");
+    }
+}

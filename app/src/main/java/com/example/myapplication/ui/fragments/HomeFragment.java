8f19583d8c2dc560787b75f32d61b4f268053322
maoyiyun <maoyiyunchris@qq.com>
2021-10-12 17:44:37 +0800

    tried to link home page weather info with data base, but encountered error

--- app/src/main/java/com/example/myapplication/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/fragments/HomeFragment.java
@@ -1,25 +1,41 @@
 package com.example.myapplication.ui.fragments;
 
+import static com.example.myapplication.json.weather.Weather.AIR_TEMPERATURE_JSON_URL;
+import static com.example.myapplication.json.weather.Weather.PM25_JSON_URL;
+import static com.example.myapplication.json.weather.Weather.RAINFALL_JSON_URL;
+import static com.example.myapplication.json.weather.Weather.RELATIVE_HUMIDITY_JSON_URL;
+import static com.example.myapplication.json.weather.Weather.UV_INDEX_JSON_URL;
+import static com.example.myapplication.json.weather.Weather.WEATHER_FORECAST_JSON_URL;
+import static com.example.myapplication.json.weather.Weather.WIND_DIRECTION_JSON_URL;
+import static com.example.myapplication.json.weather.Weather.WIND_SPEED_JSON_URL;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
+import com.example.myapplication.beans.Coordinates;
+import com.example.myapplication.json.weather.Weather;
 import com.example.myapplication.ui.activities.AddPlanActivity;
 import com.example.myapplication.ui.activities.CheckInActivity;
 import com.example.myapplication.ui.activities.SelectSportActivity;
+import com.example.myapplication.utils.IOUtil;
 
 public class HomeFragment extends Fragment {
     View view;
     Button mMakePlanButton;
     Button mCheckInButton;
+    TextView temperature, pm25,uvIndex, humidity, forecast;
+    String temperature_string, pm25_string, uvIndex_string, humidity_string, forecast_string;
+    Coordinates temp;
 
     @Nullable
     @Override
@@ -42,6 +58,36 @@ public class HomeFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        temperature = view.findViewById(R.id.temperature);
+        pm25 = view.findViewById(R.id.pm25_value);
+        uvIndex = view.findViewById(R.id.UV_value);
+        humidity = view.findViewById(R.id.Humidity_value);
+        forecast = view.findViewById(R.id.Forecast);
+
+        Weather weather = new Weather(
+                IOUtil.readFromURL(AIR_TEMPERATURE_JSON_URL),
+                IOUtil.readFromURL(RAINFALL_JSON_URL),
+                IOUtil.readFromURL(RELATIVE_HUMIDITY_JSON_URL),
+                IOUtil.readFromURL(WIND_DIRECTION_JSON_URL),
+                IOUtil.readFromURL(WIND_SPEED_JSON_URL),
+                IOUtil.readFromURL(UV_INDEX_JSON_URL),
+                IOUtil.readFromURL(PM25_JSON_URL),
+                IOUtil.readFromURL(WEATHER_FORECAST_JSON_URL));
+
+        temp = new Coordinates(1,104,"test_location");
+
+        temperature_string = weather.getWeatherData(temp).getTemperature().getResult().toString();
+        pm25_string = weather.getWeatherData(temp).getPM25().getResult().toString();
+        uvIndex_string = weather.getWeatherData(temp).getUVIndex().toString();
+        humidity_string = weather.getWeatherData(temp).getRelativeHumidity().getResult().toString();
+        forecast_string = weather.getWeatherData(temp).getForecast().getResult();
+
+        temperature.setText(temperature_string);
+        pm25.setText(pm25_string);
+        uvIndex.setText(uvIndex_string);
+        humidity.setText(humidity_string);
+        forecast.setText(forecast_string);
 
         return view;
     }

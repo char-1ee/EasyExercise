06e76df3c682c34e95e2525e2337c19ce015dac4
Char1ee <xingjianli59@gmail.com>
2021-10-17 05:50:14 +0800

    feat: update SignInActivity.java, add email/password sign-in choice

--- app/src/main/java/com/example/myapplication/ui/activities/SignInActivity.java
+++ app/src/main/java/com/example/myapplication/ui/activities/SignInActivity.java
@@ -2,9 +2,15 @@ package com.example.myapplication.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
@@ -14,15 +20,20 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 1234;
 
+    private EditText inputEmail, inputPassword;
+    private Button signInButton, signUpButton, resetPasswordButton;
+    private ProgressBar progressBar;
     private GoogleSignInClient mSignInClient;
     private FirebaseAuth mFirebaseAuth;
     private SignInButton mGoogleSignInButton;
@@ -31,8 +42,7 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        mGoogleSignInButton = findViewById(R.id.google_sign_in_button);
-        mGoogleSignInButton.setOnClickListener(view -> signIn());
+        initView();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id_hardcode))
@@ -41,6 +51,68 @@ public class SignInActivity extends AppCompatActivity {
         mSignInClient = GoogleSignIn.getClient(this, gso);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    private void initView() {
+        signInButton = findViewById(R.id.sign_in_button);
+        signUpButton = findViewById(R.id.sign_up_button);
+        inputEmail =  findViewById(R.id.email);
+        inputPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+        resetPasswordButton = findViewById(R.id.btn_reset_password);
+        mGoogleSignInButton = findViewById(R.id.google_sign_in_button);
+
+        signInButton.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+        resetPasswordButton.setOnClickListener(this);
+        mGoogleSignInButton.setOnClickListener(view -> signIn());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                finish();
+            case R.id.btn_reset_password:
+//                startActivity(new Intent(SignInActivity.this, ResetPasswordActivity.class));      //TODO
+            case R.id.sign_up_button:
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                //create user
+                mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignInActivity.this, task -> {
+                            Toast.makeText(SignInActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                            // If sign in fails, display a message to the user. If sign in succeeds
+                            // the auth state listener will be notified and logic to handle the
+                            // signed in user can be handled in the listener.
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(SignInActivity.this, "Authentication failed." + task.getException(),
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                                finish();
+                            }
+                        });
+
+        }
     }
 
     private void signIn() {
@@ -71,5 +143,11 @@ public class SignInActivity extends AppCompatActivity {
                 })
                 .addOnFailureListener(this, e -> Toast.makeText(SignInActivity.this, "Authentication failed.",
                         Toast.LENGTH_SHORT).show());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
     }
 }

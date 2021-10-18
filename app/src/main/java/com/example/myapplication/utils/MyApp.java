3eba89ecd0d8080c14d4cb4c871f6d7f8ff140f8
Char1ee <xingjianli59@gmail.com>
2021-10-18 17:34:46 +0800

    doc: MyApp.java

--- app/src/main/java/com/example/myapplication/utils/MyApp.java
+++ app/src/main/java/com/example/myapplication/utils/MyApp.java
@@ -3,6 +3,12 @@ package com.example.myapplication.utils;
 import android.app.Application;
 import android.content.Context;
 
+/**
+ *  Utility class to get application context statically.
+ *
+ * @author Li Xingjian
+ * @see com.example.myapplication.databases.DatabaseHelper
+ */
 public class MyApp extends Application {
     public static Context context = null;
 

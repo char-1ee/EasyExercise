126ceac933f9ddd93ef1cff6f8bbb5dc6a00f751
maoyiyun <maoyiyunchris@qq.com>
2021-11-01 17:18:05 +0800

    fixed user fragment

---
+++ app/src/main/java/com/example/myapplication/beans/beans/CustomizedLocation.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.beans.beans;
+
+public class CustomizedLocation extends Location {
+    public CustomizedLocation(Coordinates coordinates) {
+        super(coordinates, false);
+    }
+}
